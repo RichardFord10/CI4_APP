e651f3561c0d983dc9b7aa666fe99cc6fc7b5c11
Richard Ford <36119878+RichardFord10@users.noreply.github.com>
2022-06-10 17:50:03 -0400

    starting assets config

--- project-root/app/Views/templates/footer.php
+++ project-root/app/Views/templates/footer.php
@@ -1,3 +1,4 @@
-<em>&copy; 2021</em>
-</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="js/scripts.js"></script>
+    </body>
 </html>
--- project-root/app/Views/templates/header.php
+++ project-root/app/Views/templates/header.php
@@ -1,8 +1,13 @@
-<!doctype html>
-<html>
-<head>
-    <title>CodeIgniter Tutorial</title>
-</head>
-<body>
-
-    <h1><?= esc($title) ?></h1>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title><?php esc($title); ?></title>
+        <link src="<?php echo base_url('modules/admin/css/styles.css'); ?>">
+        <link src="<?php echo base_url('modules/admin/js/scripts.css'); ?>">
+        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+    </head>
